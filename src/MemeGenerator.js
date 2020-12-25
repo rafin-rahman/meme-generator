05fbbe6bd0dbb9299731f607a14ff57fa9a1e1cc
Rafin Rahman <ra-fin@live.com>
2020-12-25 15:09:37 +0000

    initial commit for meme generator using react

---
+++ src/MemeGenerator.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class MemeGenerator extends Component {
+  render() {
+    return <main>This is meme gen</main>;
+  }
+}
+
+export default MemeGenerator;
